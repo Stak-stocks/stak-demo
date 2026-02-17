06394f47289f9708f9f233abea074f8135bcf185
Emmanuel Enenta <ecenenta16@my.fisk.edu>
2026-02-17 02:55:20 -0600

    update

--- frontend/src/routes/my-stak.tsx
+++ frontend/src/routes/my-stak.tsx
@@ -299,7 +299,7 @@ function MyStakPage() {
 					</p>
 				</header>
 
-				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6">
+				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1 sm:gap-6">
 					{swipedBrands.map((brand) => (
 						<div
 							key={brand.id}
